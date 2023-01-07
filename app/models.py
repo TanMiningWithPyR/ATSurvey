3a057dd28221dccaed83e5936da92d3c74d08eb2
fuyou <fuyou_tan@sina.cn>
2023-01-07 19:57:43 +0800

    code generator

--- app/models.py
+++ app/models.py
@@ -2,6 +2,8 @@ from flask_appbuilder import Model
 from sqlalchemy import Column, Integer, String, ForeignKey, Date
 from sqlalchemy.orm import relationship
 from . import db, app
+
+# from functools import partial
 """
 
 You can use the extra Flask-AppBuilder fields and Mixin's
@@ -10,40 +12,42 @@ AuditMixin will add automatic timestamp of created and modified by who
 
 
 """
-
-def key_generator():
-	record = db.session.query(Brand).order_by(Brand.id.desc()).first()
+def code_generator(model, code_length):
+	record = db.session.query(model).order_by(model.id.desc()).first()
 	try:
 		i = int(record.id) + 1
-		if i < 10:
-			id = '00' + str(i)
-		elif i < 100 :
-			id = '0' + str(i)
-		else:
-			id = str(i)
+		num_0 = code_length - len(str(i))  
+		id = num_0 * '0' + str(i)
 	except (AttributeError):
-		id = '000'
+		id = code_length * '0'
 
 	app.logger.debug("tfy:" + id)
 	return  id
 
-class Brand(Model):
-	id = Column(String(3), primary_key=True, default=key_generator)
-	name = Column(String(50), unique = True, nullable=False)
+def brand_code_generator():
+    return code_generator(Brand, 3)
 
-	def __repr__(self):
-		return self.name
+def project_code_generator():
+    return code_generator(Project, 4)
+
+class Brand(Model):
+    id = Column(String(3), primary_key=True, default=brand_code_generator) 
+    name = Column(String(50), unique = True, nullable=False)
+
+    def __repr__(self):
+        return self.name
+
 
 class Project(Model):
-	id = Column(String(6), primary_key=True)
-	name = Column(String(150), unique = True, nullable=False)
-	department = Column(String(100))
-	personal_name = Column(String(100))	
-	personal_email = Column(String(100))
-	personal_phone = Column(String(20))
-	personal_cellphone = Column(String(20))
-	brand_id = Column(String(3), ForeignKey('brand.id'))
-	brand = relationship("Brand")
+    id = Column(String(4), primary_key=True, default=project_code_generator)
+    name = Column(String(150), unique = True, nullable=False)
+    department = Column(String(100))
+    personal_name = Column(String(100))	
+    personal_email = Column(String(100))
+    personal_phone = Column(String(20))
+    personal_cellphone = Column(String(20))
+    brand_id = Column(String(3), ForeignKey('brand.id'))
+    brand = relationship("Brand")
 
-	def __repr__(self):
-		return self.name	
+    def __repr__(self):
+        return self.name	
