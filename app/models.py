c32cd6e5bb7ec154c31146c080998cd17d425bb3
Alan Tan <fuyou_tan@sina.cn>
2023-01-13 17:19:26 +0800

    model ready

--- app/models.py
+++ app/models.py
@@ -1,7 +1,13 @@
 from flask_appbuilder import Model
-from sqlalchemy import Column, Integer, String, ForeignKey, Date
+from flask_appbuilder.models.mixins import AuditMixin, FileColumn, ImageColumn
+from flask_appbuilder.security.sqla.models import User
+from sqlalchemy import Column, Integer, String, ForeignKey, Date, Float, Text
 from sqlalchemy.orm import relationship
+from flask import Markup, url_for
+
 from . import db, app
+
+from flask_appbuilder.filemanager import get_file_original_name, ImageManager
 
 # from functools import partial
 """
@@ -30,6 +36,30 @@ def brand_code_generator():
 def project_code_generator():
     return code_generator(Project, 4)
 
+def dealer_code_generator():
+    return code_generator(Dealer, 7)
+
+def round_code_generator():
+    return code_generator(Round, 6)
+
+def questionnaire_code_generator():
+    return code_generator(Questionnaire, 8)
+
+def questionnaire_item_code_generator():
+    return code_generator(Questionnaire, 10)
+
+def audit_code_generator():
+    return code_generator(Audit, 14)
+
+def project_file_code_generator():
+    return code_generator(ProjectFiles,12)
+
+def image_file_code_generator():
+    return code_generator(ImageFiles,12)
+
+def audit_item_code_generator():
+    return code_generator(AuditItem,16)
+
 class Brand(Model):
     id = Column(String(3), primary_key=True, default=brand_code_generator) 
     name = Column(String(50), unique = True, nullable=False)
@@ -53,17 +83,171 @@ class Project(Model):
         return self.name	
     
 class Dealer(Model):
-    id = Column(String(7), primary_key=True, default=project_code_generator)
-    
+    id = Column(String(7), primary_key=True, default=dealer_code_generator)
+    name = Column(String(50))
+    source_dealer_code = Column(String(30))
+    type = Column(String(10))
+    address = Column(String(200))
+    province = Column(String(20))
+    city = Column(String(20))
+    open_date = Column(Date)
+    area_1 = Column(String(20))
+    area_2 = Column(String(20))
+    brand_id = Column(String(3), ForeignKey('brand.id'))
+    brand = relationship("Brand")
+
+    def __repr__(self):
+        return self.name
+
 class Round(Model):
-    id = Column(String(6), primary_key=True, default=project_code_generator)
+    id = Column(String(6), primary_key=True, default=round_code_generator)
+    name = Column(String(10))
+    open_date = Column(Date)
+    close_date = Column(Date)
+    project_id = Column(String(4), ForeignKey('project.id'))
+    project = relationship("Project")
     
+    def __repr__(self):
+        return self.name
+
 class Questionnaire(Model):
-    id = Column(String(10), primary_key=True, default=project_code_generator)
+    id = Column(String(8), primary_key=True, default=questionnaire_code_generator)
+    name = Column(String(10))
+    round_id = Column(String(4), ForeignKey('round.id'))
+    round = relationship("Round")
     
+    def __repr__(self):
+        return self.name
+
 class Audit(Model):
-    id = Column(String(7), primary_key=True, default=project_code_generator)
+    id = Column(String(14), primary_key=True, default=audit_code_generator)
+    start_date = Column(Date)
+    end_date = Column(Date)
+
+    dealer_id = Column(String(7), ForeignKey('dealer.id'))
+    dealer = relationship("Dealer",  foreign_keys=dealer_id)    
+
+    questionnaire_id = Column(String(8), ForeignKey('questionnaire.id'))
+    questionnaire = relationship("Questionnaire", foreign_keys=questionnaire_id)    
+
+    audit_stage = Column(String(3))
+
+    auditor_id = Column(Integer, ForeignKey('ab_user.id'))
+    reviewer1_id = Column(Integer, ForeignKey('ab_user.id'))
+    reviewer2_id = Column(Integer, ForeignKey('ab_user.id'))
+    reviewer3_id = Column(Integer, ForeignKey('ab_user.id'))
+    auditor = relationship("User", foreign_keys=auditor_id)    
+    reviewer1 = relationship("User", foreign_keys=reviewer1_id)   
+    reviewer2 = relationship("User", foreign_keys=reviewer2_id)   
+    reviewer3 = relationship("User", foreign_keys=reviewer3_id)   
+
+    def __repr__(self):
+        return self.id
     
-class Auditor(Model):
-    id = Column(String(5), primary_key=True, default=project_code_generator)
+class QuestionnaireItem(Model):
+    id = Column(String(10), primary_key=True, default=questionnaire_item_code_generator)
+    source_level_1 = Column(String(50))
+    source_level_2 = Column(String(50))
+    source_level_3 = Column(String(50))
+    source_level_4 = Column(String(50))
+    source_level_5 = Column(String(50))
+    source_level_6 = Column(String(50))
+    my_function_level_1 = Column(String(50))
+    my_function_level_2 = Column(String(50))
+    my_function_level_3 = Column(String(50))
+    my_facility_area_code = Column(String(10))
+    my_information_code = Column(String(10))
+    my_car_lifecycle = Column(String(10))
+    my_dealer_lifecycle = Column(String(10))
+    item_code = Column(String(20))  # source_level_1_code & source_level_2_code & source_level_3_code & source_level_4_code & source_level_5_code & source_level_6_code 
+    item = Column(String(200))
+    item_score = Column(Float)
+    questionnaire_id = Column(String(8), ForeignKey('questionnaire.id'))
+    questionnaire = relationship("Questionnaire")
     
+    def __repr__(self):
+        return self.item
+
+class AuditItem(Model):
+    id = Column(String(16), primary_key=True, default=audit_item_code_generator)
+    questionnaire_item_id = Column(String(10), ForeignKey('questionnaire_item.id'))
+    questionnaire_item = relationship("QuestionnaireItem", foreign_keys = questionnaire_item_id)
+
+    audit_id = Column(String(10), ForeignKey('audit.id'))
+    audit = relationship('Audit', foreign_keys = audit_id)
+
+    # answer content for audit, review.
+    answer_choose_audit = Column(Integer)
+    answer_content_audit = Column(Text)
+
+    answer_choose_viewer1 = Column(Integer)
+    answer_content_viewer1 = Column(Text)
+
+    answer_choose_viewer2 = Column(Integer)
+    answer_content_viewer2 = Column(Text)
+
+    answer_choose_viewer3 = Column(Integer)
+    answer_content_viewer3 = Column(Text)
+
+    def __repr__(self):
+        return self.id
+
+class ProjectFiles(Model):
+    id = Column(String(12), primary_key=True, default=project_file_code_generator)
+    audit_item_id = Column(String(16), ForeignKey("audit_item.id"))
+    audit_item = relationship("AuditItem")
+    file = Column(FileColumn, nullable=False)
+    description = Column(String(150))
+
+    def download(self):
+        return Markup(
+            '<a href="'
+            + url_for("ProjectFilesView.download", filename=str(self.file))
+            + '">Download</a>'
+        )
+
+    def file_name(self):
+        return get_file_original_name(str(self.file))
+
+class ImageFiles(Model):
+    id = Column(String(12), primary_key=True, default=image_file_code_generator)
+    audit_item_id = Column(String(16), ForeignKey("audit_item.id"))
+    audit_item = relationship("AuditItem")
+    photo = Column(ImageColumn(thumbnail_size=(30, 30, True), size=(300, 300, True)))
+    description = Column(String(150))
+
+    def photo_img(self):
+        im = ImageManager()
+        if self.photo:
+            return Markup(
+                '<a href="' +
+                url_for("ImageFilesView.show", pk=str(self.id)) +
+                '" class="thumbnail"><img src="' +
+                im.get_url(self.photo) +
+                '" alt="Photo" class="img-rounded img-responsive"></a>'
+            )
+        else:
+            return Markup(
+                '<a href="' +
+                url_for("ImageFilesView.show", pk=str(self.id)) +
+                '" class="thumbnail"><img src="//:0" alt="Photo" class="img-responsive">'
+                '</a>'
+            )
+
+    def photo_img_thumbnail(self):
+        im = ImageManager()
+        if self.photo:
+            return Markup(
+                '<a href="' +
+                url_for("ImageFilesView.show", pk=str(self.id)) +
+                '" class="thumbnail"><img src="' +
+                im.get_url_thumbnail(self.photo) +
+                '" alt="Photo" class="img-rounded img-responsive"></a>'
+            )
+        else:
+            return Markup(
+                '<a href="' +
+                url_for("ImageFilesView.show", pk=str(self.id)) +
+                '" class="thumbnail"><img src="//:0" alt="Photo" class="img-responsive">'
+                '</a>'
+            )
--- app/views.py
+++ app/views.py
@@ -1,9 +1,9 @@
 from flask import render_template
 from flask_appbuilder.models.sqla.interface import SQLAInterface
-from flask_appbuilder import ModelView, ModelRestApi
+from flask_appbuilder.views import ModelView, CompactCRUDMixin
 
 from . import appbuilder, db
-from .models import  Brand, Project
+from .models import  Brand, Project, Dealer, Round, Audit, Questionnaire, QuestionnaireItem, AuditItem, ImageFiles, ProjectFiles
 
 """
     Create your Model based REST API::
@@ -47,6 +47,128 @@ def page_not_found(e):
         404,
     )
 
+class ProjectFilesView(ModelView):
+    datamodel = SQLAInterface(ProjectFiles)
+
+    list_columns = ['id', 'audit_item', 'file', 'description']
+    add_columns = ['id', 'audit_item', 'file', 'description'] 
+
+class ImageFilesView(ModelView):
+    datamodel = SQLAInterface(ImageFiles)
+
+    list_columns = ['id', 'audit_item', 'description', 'photo']
+   # , 'photo_img_thumbnail']
+    add_columns = ['id', 'audit_item', 'description', 'photo']
+   # , 'photo_img_thumbnail']
+
+class AuditItemView(ModelView):
+    datamodel = SQLAInterface(AuditItem)
+
+    # show_template = "appbuilder/general/model/show_cascade.html"
+    # edit_template = "appbuilder/general/model/edit_cascade.html"
+
+    list_columns = ['audit', 'questionnaire_item', 'answer_choose_audit','answer_content_audit',
+    'answer_choose_viewer1','answer_content_viewer1',
+    'answer_choose_viewer2','answer_content_viewer2',
+    'answer_choose_viewer3','answer_content_viewer3']
+    add_columns = ['audit', 'questionnaire_item', 'answer_choose_audit','answer_content_audit',
+    'answer_choose_viewer1','answer_content_viewer1',
+    'answer_choose_viewer2','answer_content_viewer2',
+    'answer_choose_viewer3','answer_content_viewer3']
+      
+    related_views = [ImageFilesView, ProjectFilesView]  
+
+class AuditView(ModelView):
+    datamodel = SQLAInterface(Audit)
+
+    list_columns = ['id', 'start_date', 'end_date', 'dealer', 'questionnaire', 'audit_stage', 'auditor','reviewer1','reviewer2','reviewer3']
+    add_columns = ['id', 'start_date', 'end_date', 'dealer', 'questionnaire', 'audit_stage', 'auditor','reviewer1','reviewer2','reviewer3']
+
+    show_fieldsets = [
+        (
+            'Summary',
+            {'fields': ['start_date', 'end_date', 'dealer','questionnaire','audit_stage']}
+        ),
+        (
+            'Audit Info',
+            {'fields': ['auditor','reviewer1','reviewer2','reviewer3'], 'expanded': False}
+        ),
+    ]
+
+    related_views = [AuditItemView]  
+
+class QuestionnaireItemView(ModelView):
+    datamodel = SQLAInterface(QuestionnaireItem)
+
+    list_columns = ['id','questionnaire','source_level_1','source_level_2','source_level_3','source_level_4','source_level_5','source_level_6',
+    'my_function_level_1','my_function_level_2','my_function_level_3','my_facility_area_code','my_information_code','my_car_lifecycle','my_dealer_lifecycle',
+    'item_code','item', 'item_score']
+    add_columns = ['id','questionnaire','source_level_1','source_level_2','source_level_3','source_level_4','source_level_5','source_level_6',
+    'my_function_level_1','my_function_level_2','my_function_level_3','my_facility_area_code','my_information_code','my_car_lifecycle','my_dealer_lifecycle',
+    'item_code','item', 'item_score']
+
+    show_fieldsets = [
+        (
+            'Summary',
+            {'fields': ['name', 'questionnaire', 'source_level_1','source_level_2','source_level_3','source_level_4','source_level_5','source_level_6']}
+        ),
+        (
+            'Questionnaire Item Info',
+            {'fields': ['item_code','item', 'item_score', 'my_function_level_1','my_function_level_2','my_function_level_3','my_facility_area_code','my_information_code','my_car_lifecycle','my_dealer_lifecycle'], 'expanded': False}
+        ),
+    ]
+
+    related_views = [AuditItemView] 
+
+class QuestionnaireView(ModelView):
+    datamodel = SQLAInterface(Questionnaire)
+
+    list_columns = ['id', 'name','round']
+    add_columns = ['id', 'name', 'round']
+      
+    related_views = [QuestionnaireItemView, AuditView]  
+
+class RoundModelView(ModelView):
+    datamodel = SQLAInterface(Round)
+
+    label_columns = {'project':'Project'}
+    add_columns = ['id','project','name','open_date','close_date']
+    list_columns = ['id','project', 'name','open_date','close_date']
+
+    show_fieldsets = [
+        (
+            'Summary',
+            {'fields': ['name', 'project']}
+        ),
+        (
+            'Round Info',
+            {'fields': ['open_date', 'close_date'], 'expanded': False}
+        ),
+    ]
+
+    related_views = [QuestionnaireView]  
+
+
+class DealerModelView(ModelView):
+    datamodel = SQLAInterface(Dealer)
+
+    label_columns = {'brand':'Brand'}
+    add_columns = ['id','brand','type','source_dealer_code','name','address','province','city','open_date','area_1','area_2']
+    list_columns = ['id','brand','type','source_dealer_code','name','address','province','city','open_date','area_1','area_2']
+
+    show_fieldsets = [
+        (
+            'Summary',
+            {'fields': ['name', 'source_dealer_code', 'brand','type']}
+        ),
+        (
+            'Dealer Info',
+            {'fields': ['address','province','city','open_date','area_1','area_2'], 'expanded': False}
+        ),
+    ]
+
+    #related_views = [QuestionnaireView]
+
 class ProjectModelView(ModelView):
     datamodel = SQLAInterface(Project)
 
@@ -65,13 +187,16 @@ class ProjectModelView(ModelView):
         ),
     ]
 
+    related_views = [RoundModelView]
+
 class BrandModelView(ModelView):
     datamodel = SQLAInterface(Brand)
     list_columns = ['id', 'name']
     add_columns = ['id', 'name']
-    related_views = [ProjectModelView]
+    related_views = [ProjectModelView, DealerModelView]
 
 db.create_all()
+
 appbuilder.add_view(
     BrandModelView,
     "List Brands",
@@ -79,9 +204,63 @@ appbuilder.add_view(
     category = "Brands",
     category_icon = "fa-car"
 )
+
 appbuilder.add_view(
     ProjectModelView,
     "List Projects",
     icon = "fa-folder-open-o",
     category = "Brands"
+)
+
+appbuilder.add_view(
+    DealerModelView,
+    "List Dealers",
+    icon = "fa-folder-open-o",
+    category = "Brands"
+)
+
+appbuilder.add_view(
+    RoundModelView,
+    "List Rounds",
+    icon = "fa-folder-open-o",
+    category = "Questionnaires",
+    category_icon = "fa-car"
+)
+
+appbuilder.add_view(
+    QuestionnaireView,
+    "List Questionnaires",
+    icon = "fa-car",
+    category = "Questionnaires"
+)
+
+appbuilder.add_view(
+    QuestionnaireItemView,
+    "List Questionnaire Item",
+    icon = "fa-car",
+    category = "Questionnaires"
+)
+
+appbuilder.add_view(
+    AuditView,
+    "List Audit",
+    icon = "fa-car",
+    category = "Audit"
+)
+
+appbuilder.add_view(
+    AuditItemView,
+    "List Audit Item",
+    icon = "fa-car",
+    category = "Audit"
+)
+
+# appbuilder.add_view_no_menu(ImageFilesView)
+appbuilder.add_view_no_menu(ProjectFilesView)
+
+appbuilder.add_view(
+    ImageFilesView,
+    "List Image Files",
+    icon = "fa-car",
+    category = "Audit"
 )
